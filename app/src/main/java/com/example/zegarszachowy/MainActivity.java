d909a71d760e2b5e12c45b7974b97a8edad0769e
Kajerkooo2 <prokajerko79@gmail.com>
2026-03-31 09:08:44 +0200

    Pelna apka podlaczenie w MainActivity

--- app/src/main/java/com/example/zegarszachowy/MainActivity.java
+++ app/src/main/java/com/example/zegarszachowy/MainActivity.java
@@ -16,7 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnUser1 = findViewById(R.id.User1);
+        btnUser2 = findViewById(R.id.User2);
 
+        user = new User(btnUser1, btnUser2);
+
+        btnUser1.setOnClickListener(v -> {
+            if (!isGameRunning) {
+                isGameRunning = true;
+                user.startTimer();
+            } else if (user.isUser1Active()) {
+                user.switchPlayer();
+            }
+        });
+
+        btnUser2.setOnClickListener(v -> {
+            if (!isGameRunning) {
+                isGameRunning = true;
+                user.startTimer();
+            } else if (!user.isUser1Active()) {
+                user.switchPlayer();
+            }
+        });
     }
 
 
